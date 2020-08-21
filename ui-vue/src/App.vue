8e1a0dfa5cef1d3411ad395f0fb7cf9cdf79e8e2
严俊东 <1172482914@qq.com>
2020-08-21 08:46:18 +0800

    使用socket

--- ui-vue/src/App.vue
+++ ui-vue/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <div class="container-ui" v-loading="loading">
-      <h4>基本信息</h4>
+      <h4>基本信息 {{ws && ws.readyState}}</h4>
+      <div v-if="readyState === 0">连接中</div>
+      <div v-if="readyState === 1">已连接</div>
+      <div v-if="readyState === 2">关闭中</div>
+      <div v-if="readyState === 3">未连接</div>
       <BaseForm ref="baseForm" />
       <h4>数据结构</h4>
       <TableList ref="tableList" />
       <div style="margin-top: 20px; float: right">
-        <el-button type="primary" @click="handleSave">保存</el-button>
+        <el-button type="primary" :disabled="readyState !== 3" @click="connect">连接</el-button>
+        <el-button type="primary" :disabled="readyState !== 1" @click="closeSocket">断开连接</el-button>
+        <el-button type="primary" :disabled="readyState !== 1" @click="handleSave">保存</el-button>
       </div>
     </div>
   </div>
@@ -15,7 +21,14 @@
 <script>
 import BaseForm from "./components/BaseForm";
 import TableList from "./components/TableList";
-import axios from "axios";
+const parseMsgToObject = msg => {
+  try {
+    return JSON.parse(msg);
+  } catch (err) {
+    return msg;
+  }
+};
+
 export default {
   name: "App",
   components: {
@@ -24,30 +37,72 @@ export default {
   },
   data() {
     return {
-      loading: true
+      ws: null,
+      readyState: 2,
+      connecting: false,
+      connected: false
     };
   },
+  computed: {
+    loading() {
+      return this.readyState === 0 || this.readyState === 2;
+    }
+  },
   created() {
-    axios.get("/config").then(res => {
-      this.loading = false;
-      const {
-        dirPath = "",
-        mongoLink = "",
-        adminName = "",
-        tableList = []
-      } = res.data;
-      this.$refs["baseForm"].setData({ dirPath, mongoLink, adminName });
-      this.$refs["tableList"].setData(tableList);
-    });
+    this.connect();
   },
   methods: {
+    closeSocket() {
+      if (this.ws) {
+        this.ws.close();
+      }
+    },
+    emit(event, data) {
+      if (this.ws) {
+        this.ws.send(JSON.stringify({ event, payload: data }));
+      }
+    },
+    connect() {
+      const _up = () => (this.readyState = this.ws.readyState);
+      if (this.ws) {
+        this.ws.close();
+      }
+      this.connecting = true;
+      this.ws = new WebSocket(`ws://localhost:9191/ws`);
+      _up();
+      this.ws.onerror = () => {
+        this.$message.error("连接失败，检查地址是否正确、项目是否启动！");
+      };
+      this.ws.onmessage = messageEvent => {
+        const data = parseMsgToObject(messageEvent.data);
+        if (typeof data === "object") {
+          console.log(data);
+          if (data.event === "config update" || data.event === "get config") {
+            const {
+              dirPath = "",
+              mongoLink = "",
+              adminName = "",
+              tableList = []
+            } = data.payload;
+            this.$refs["baseForm"].setData({ dirPath, mongoLink, adminName });
+            this.$refs["tableList"].setData(tableList);
+          }
+        } else {
+          console.error("未正常解析消息");
+        }
+      };
+      this.ws.onopen = () => {
+        _up();
+        this.emit("get config");
+      };
+      this.ws.onclose = () => {
+        _up();
+      };
+    },
     handleSave() {
       this.$refs["baseForm"].submitForm(data => {
         const tableList = this.$refs["tableList"].tableData;
-        this.loading = true;
-        axios.post("/config", { ...data, tableList }).then(() => {
-          this.loading = false;
-        });
+        this.emit("save config", { ...data, tableList });
       });
     }
   }
